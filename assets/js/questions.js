140fe7961f03252fcabde9e44ff66fdcdaf7e6f0
elmfer <elmfer11@outlook.com>
2023-10-01 23:43:18 -0500

    added two more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,4 @@
-// Array of possible questions stored here
+// Array of possible questions are stored here
 
 const questions = [
   {
@@ -7,5 +7,19 @@ const questions = [
       "Number", "String", "BigInt", "undefined"
     ],
     answer: 0 
+  },
+  {
+    question: "Creating a variable equal to \'{}\' with key value pairs inside is called a/an ______.",
+    choices: [
+      "array", "object", "function", "map"
+    ],
+    answer: 1
+  },
+  {
+    question: "Centering a div was always hard, not until ______ were introducted in CSS.",
+    choices : [
+      "float", "selectors", "media queries", "flex boxes"
+    ],
+    answer: 3
   }
 ]

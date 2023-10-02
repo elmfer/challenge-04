a77e56b5139dfa50e2dbc23c2d125807b8dcfb11
elmfer <elmfer11@outlook.com>
2023-10-01 22:51:42 -0500

    Added question list file

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+// Array of possible questions stored here
+
+const questions = [
+  {
+    question: "What type is \'Nan\'?",
+    choices: [
+      "Number", "String", "BigInt", "undefined"
+    ],
+    answer: 0 
+  }
+]
